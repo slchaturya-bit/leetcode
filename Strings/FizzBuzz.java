842ec51aad30f5848d718a556308beb5c10f2ef6
Lakshmi Chaturya <slchaturya@gmail.com>
2026-07-30 22:02:18 +0530

    added new file

---
+++ Strings/FizzBuzz.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<>();
+       for (int i = 1; i <= n; i++) {
+    // check i
+    if (i % 3 == 0 && i % 5 == 0){
+        answer.add("FizzBuzz");
+
+    }
+    else if (i % 3 == 0){
+        answer.add("Fizz");
+
+    }
+    else if(i%5==0){
+        answer.add("Buzz");
+    }else {
+    answer.add(String.valueOf(i));
+}
+
+}
+return answer;//main logic is when we encounter multiple of 3 display fizz and buzz when 5 is multiplle
+
+    }
+    }
